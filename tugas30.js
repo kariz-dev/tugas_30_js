dce1d53adb3f975c11795c47f5e557167d572148
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-26 02:32:52 +0700

    tugas 30 js

---
+++ tugas30.js
@@ -0,0 +1,10 @@
+/* Tugas 30 Javascript */
+// Muhammad Rizwan
+
+function tugasTigapuluh() {
+    let str = "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat";
+
+    console.log(str.match(/[es]/g));
+}
+
+tugasTigapuluh();
